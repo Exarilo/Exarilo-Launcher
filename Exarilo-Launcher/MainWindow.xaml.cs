48fc93d4d33063caa31217b24db2068f7708a145
Exarilo <romain.madery92@gmail.com>
2024-04-23 01:08:13 +0200

    Add project files.

---
+++ Exarilo-Launcher/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Exarilo_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private const int WmSyscommand = 0x112;
+        private HwndSource _hwndSource;
+
+        private enum ResizeDirection
+        {
+            Left = 61441,
+            Right = 61442,
+            Top = 61443,
+            TopLeft = 61444,
+            TopRight = 61445,
+            Bottom = 61446,
+            BottomLeft = 61447,
+            BottomRight = 61448,
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+
+        public MainWindow()
+        {
+            SourceInitialized += Window1_SourceInitialized;
+            InitializeComponent();
+            ButtonsFrame.Content = new ButtonsPage();
+        }
+        private void Window_OnMouseDown(object sender, MouseButtonEventArgs e) { if (e.LeftButton == MouseButtonState.Pressed) Application.Current.MainWindow.DragMove(); }
+        private void CloseButton_Click(object sender, RoutedEventArgs e) { Application.Current.Shutdown(); }
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e) { AdjustWindowSize(); }
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) { Application.Current.MainWindow.WindowState = WindowState.Minimized; }
+        private static void AdjustWindowSize() { Application.Current.MainWindow.WindowState = Application.Current.MainWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized; }
+        private void Window1_SourceInitialized(object sender, EventArgs e) { _hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource; }
+        private void ResizeWindow(ResizeDirection direction) { SendMessage(_hwndSource.Handle, WmSyscommand, (IntPtr)direction, IntPtr.Zero); }
+
+
+        protected void ResetCursor(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton != MouseButtonState.Pressed)
+            {
+                Cursor = Cursors.Arrow;
+            }
+        }
+
+        protected void Resize(object sender, MouseButtonEventArgs e)
+        {
+            var clickedShape = sender as Shape;
+
+            if (clickedShape == null) return;
+            switch (clickedShape.Name)
+            {
+                case "ResizeN":
+                    Cursor = Cursors.SizeNS;
+                    ResizeWindow(ResizeDirection.Top);
+                    break;
+                case "ResizeE":
+                    Cursor = Cursors.SizeWE;
+                    ResizeWindow(ResizeDirection.Right);
+                    break;
+                case "ResizeS":
+                    Cursor = Cursors.SizeNS;
+                    ResizeWindow(ResizeDirection.Bottom);
+                    break;
+                case "ResizeW":
+                    Cursor = Cursors.SizeWE;
+                    ResizeWindow(ResizeDirection.Left);
+                    break;
+                case "ResizeNW":
+                    Cursor = Cursors.SizeNWSE;
+                    ResizeWindow(ResizeDirection.TopLeft);
+                    break;
+                case "ResizeNE":
+                    Cursor = Cursors.SizeNESW;
+                    ResizeWindow(ResizeDirection.TopRight);
+                    break;
+                case "ResizeSE":
+                    Cursor = Cursors.SizeNWSE;
+                    ResizeWindow(ResizeDirection.BottomRight);
+                    break;
+                case "ResizeSW":
+                    Cursor = Cursors.SizeNESW;
+                    ResizeWindow(ResizeDirection.BottomLeft);
+                    break;
+            }
+        }
+
+        protected void DisplayResizeCursor(object sender, MouseEventArgs e)
+        {
+            var clickedShape = sender as Shape;
+
+            if (clickedShape == null) return;
+            switch (clickedShape.Name)
+            {
+                case "ResizeN":
+                case "ResizeS":
+                    Cursor = Cursors.SizeNS;
+                    break;
+                case "ResizeE":
+                case "ResizeW":
+                    Cursor = Cursors.SizeWE;
+                    break;
+                case "ResizeNW":
+                case "ResizeSE":
+                    Cursor = Cursors.SizeNWSE;
+                    break;
+                case "ResizeNE":
+                case "ResizeSW":
+                    Cursor = Cursors.SizeNESW;
+                    break;
+            }
+        }
+    }
+}
